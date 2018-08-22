9f88b4fa5edc0f0bf7d0dada97e76252bf70387c
Joanna Gora <joanna.gora@protonmail.com>
2018-08-22 14:31:41 +0100

    add data in seeds

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -5,8 +5,16 @@ import models.Borrower;
 
 public class Seeds {
     public static void seedData() {
+        DBHelper.deleteAll(Book.class);
+        DBHelper.deleteAll(Borrower.class);
+
         Book bookHobbit = new Book("Hobbit", "J.R.R. Tolkien");
         DBHelper.save(bookHobbit);
+        Book bookLordOfTheRigns = new Book("Lord Of the Rings", "J.R.R. Tolkien");
+        DBHelper.save(bookLordOfTheRigns);
+        Book bookFinansowyNinja = new Book("Finansowy Ninja", "Michal Szafranski");
+        DBHelper.save(bookFinansowyNinja);
+
         Borrower borrowerMike = new Borrower("Mike");
         DBHelper.save(borrowerMike);
     }
