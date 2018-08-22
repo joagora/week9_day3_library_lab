cdc7ea88f0f477467e6bb1af65924740c5dea804
Joanna Gora <joanna.gora@protonmail.com>
2018-08-22 14:44:34 +0100

    create index route in the LibraryController

--- src/main/java/controllers/LibraryController.java
+++ src/main/java/controllers/LibraryController.java
@@ -1,9 +1,21 @@
 package controllers;
 
 import db.Seeds;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 public class LibraryController {
     public static void main(String[] args) {
         Seeds.seedData();
+
+        get("/library", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "/templates/library/index.vtl");
+            return new ModelAndView(model, "/templates/layout.vtl");
+        }, new VelocityTemplateEngine());
     }
 }
